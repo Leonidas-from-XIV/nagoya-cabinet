321eac20e91608c896a8876e49a38b91d82eb9fa
Marek Kubica <marek@xivilization.net>
2014-04-13 22:11:42 +0200

    Some comments

--- external-sort/sort.rs
+++ external-sort/sort.rs
@@ -2,41 +2,59 @@ extern crate collections;
 use std::io::{File, Open, Write, TempDir};
 use collections::priority_queue::PriorityQueue;
 
+/*
+ * an entry is the number that we read and the file it came from, so after we took
+ * out one Entry from the priority queue, we need to know where to take the next number
+ * from.
+ */
 struct Entry {
 	value: u64,
 	source: File,
 }
 
+/* The priority queue depends on the ordering relation and always returns the biggest */
 impl Ord for Entry {
+	// lesser than implementation
 	fn lt(&self, other: &Entry) -> bool {
+		// we invert: for us the smallest will have the highest priority
 		!(self.value < other.value)
 	}
 }
 
+/* ordering needs equality for some reason, so there */
 impl Eq for Entry {
 	fn eq(&self, other: &Entry) -> bool {
 		self.value == other.value
 	}
 }
 
+/* A struct to hold our queue */
 struct PriorityFile {
-	queue: ~PriorityQueue<Entry>,
+	queue: PriorityQueue<Entry>,
 }
 
+/*
+ * 'Class' that takes a list of files and does a k-way merge of the smallest values
+ * from the files
+ */
 impl PriorityFile {
 	pub fn new(files: Vec<File>) -> PriorityFile {
 		let mut queue: PriorityQueue<Entry> = PriorityQueue::new();
+		// now that we got the queue, populate it with initial values
+
+		// move_iter is required to get the values themselves, not borrowed pointers to them
 		for mut f in files.move_iter() {
+			// read from file, append to queue
 			let number = match f.read_le_u64() {
 				Ok(num) => num,
 				Err(e) => fail!("failed to read u64 from file: {}", e),
 			};
-			let entry = Entry {value: number, source: f};
-			queue.push(entry);
-		}
-		PriorityFile {queue: ~queue}
+			queue.push(Entry {value: number, source: f});
+		};
+		PriorityFile {queue: queue}
 	}
 
+	/* Returns the next smallest number from the list of files or None if exhausted */
 	pub fn next(&mut self) -> Option<u64> {
 		match self.queue.maybe_pop() {
 			Some(mut res) => {
@@ -66,6 +84,8 @@ fn externalSort(mut fdInput: File, size: u64, mut fdOutput: File, memSize: u64) 
 	}
 
 	// let's create a temporary directory to store the sorted chunks
+	// neat: TempDir deletes the directory and its contents when it goes out of
+	// scope
 	let overflow_dir = match TempDir::new("externalsort") {
 		Some(temp_dir) => temp_dir,
 		None => fail!("creation of temporary directory"),
@@ -73,7 +93,11 @@ fn externalSort(mut fdInput: File, size: u64, mut fdOutput: File, memSize: u64) 
 	let overflow_path = overflow_dir.path();
 	println!("temp dir path {}", overflow_path.display());
 
+	// start the sorting runs
 	for n in range(0, runs) {
+		// iterate over our 'run' buffer and read from the input file
+		// I suppose the values are little endian, which considering we are most
+		// likely on x86, is a reasonable assumption
 		for element in run.mut_iter() {
 			let number = match fdInput.read_le_u64() {
 				Ok(num) => num,
@@ -81,12 +105,15 @@ fn externalSort(mut fdInput: File, size: u64, mut fdOutput: File, memSize: u64) 
 			};
 			*element = number;
 		}
+		// O(n log n) sort from the stdlib, hopefully more or less equivalent to
+		// C++ std::sort
 		run.sort();
 
+		// now write out the results into numbered files
 		let file_path = overflow_path.join(n.to_str());
 		let mut file_file = match File::open_mode(&file_path, Open, Write) {
 			Ok(f) => f,
-			Err(e) => fail!("output file error: {}", e),
+			Err(e) => fail!("overflow file failed opening for write: {}", e),
 		};
 
 		for &element in run.iter() {
@@ -97,22 +124,27 @@ fn externalSort(mut fdInput: File, size: u64, mut fdOutput: File, memSize: u64) 
 		};
 	};
 
+	/* sorting done, now merging */
+
+	// collect all file handles. Hopefully less than 4096
 	let mut files = Vec::with_capacity(runs as uint);
 	for n in range(0, runs) {
 		let file_path = overflow_path.join(n.to_str());
 		let fd = match File::open(&file_path) {
 			Ok(f) => f,
-			Err(e) => fail!("overflow file open error {}", e),
+			Err(e) => fail!("overflow file opening error {}", e),
 		};
 		files.push(fd);
 	}
 
+	// we got a nice class here that spits out sorted numbers until exhausted
 	let mut pf = PriorityFile::new(files);
 	loop {
 		match pf.next() {
+			// if we got a number, write it to the output
 			Some (number) => match fdOutput.write_le_u64(number) {
 				Ok(_) => (),
-				Err(e) => fail!("Didn't write to file {}", e),
+				Err(e) => fail!("writing output file failed: {}", e),
 			},
 			None => break,
 		};
